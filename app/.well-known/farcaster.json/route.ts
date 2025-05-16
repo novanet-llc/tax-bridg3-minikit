27461b39bb3bfa47450cec883950981049bc1d65
Rivara Tyr <rivara.tyr@gmail.com>
2025-05-16 09:16:34 +0200

    Preparing for Coinbase release.

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -1,5 +1,6 @@
 export async function GET() {
   const URL = process.env.NEXT_PUBLIC_URL;
+  const AWS_CDN_URL = process.env.NEXT_PUBLIC_AWS_CDN_URL;
 
   return Response.json({
     accountAssociation: {
@@ -17,6 +18,26 @@ export async function GET() {
       splashImageUrl: process.env.NEXT_PUBLIC_SPLASH_IMAGE_URL,
       splashBackgroundColor: `#${process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR}`,
       webhookUrl: `${URL}/api/webhook`,
+      subtitle: process.env.NEXT_PUBLIC_SUBTITLE,
+      description: process.env.NEXT_PUBLIC_DESCRIPTION,
+      screenshotUrls: [
+        `${AWS_CDN_URL}/taxbridg3-screen-1.png`,
+        `${AWS_CDN_URL}/taxbridg3-screen-2.png`,
+        `${AWS_CDN_URL}/taxbridg3-screen-3.png`
+      ],
+      primaryCategory: "finance",
+      tags: [
+        "tax compliance",
+        "transaction history",
+        "tax filing",
+        "tax report",
+        "tax company profile"
+      ],
+      heroImageUrl: `${AWS_CDN_URL}/taxbridg3-hero.png`,
+      tagline: process.env.NEXT_PUBLIC_TAGLINE,
+      ogTitle: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
+      ogDescription: process.env.NEXT_PUBLIC_DESCRIPTION,
+      ogImageUrl: `${AWS_CDN_URL}/taxbridg3-hero.png`
     },
   });
 }
